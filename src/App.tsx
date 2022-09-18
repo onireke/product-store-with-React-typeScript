6de52cd0fe9449d3ebb7aca1a716e14a6297348a
amosa abdulfatah olayimka <amosaabdulfatah@gmail.com>
2022-09-18 03:41:54 +0100

    feat: update

--- src/App.tsx
+++ src/App.tsx
@@ -9,23 +9,26 @@ interface Product {
   image: string;
   category: string;
   price: number;
+  description: string;
 }
 function App() {
   const [products, setProducts] = useState<Product[]>([]);
   const [errorMessage, setErrorMessage] = useState<string>("");
-  const [loading, setLoading] = useState(false);
+  const [loading, setLoading] = useState<boolean>(false);
+  const [readMore, setReadMore] = useState<boolean>(false);
   useEffect(() => {
     setLoading(true);
     setErrorMessage("");
     axios("https://fakestoreapi.com/products")
       .then(({ data }: { data: Product[] }) => {
         setProducts(
-          data.map(({ id, title, image, category, price }) => ({
+          data.map(({ id, title, image, category, price, description }) => ({
             id,
             title,
             image,
             category,
             price,
+            description,
           }))
         );
       })
@@ -47,17 +50,23 @@ function App() {
     <main>
       <h1>Product Store</h1>
       <div className="products">
-        {errorMessage && "Error occured"}
+        {errorMessage && "Error occurred"}
         {loading ? (
           <Loader />
         ) : !loading && !errorMessage && products.length < 1 ? (
           "No result"
         ) : (
-          products.map(({ id, title, image, category, price }) => (
+          products.map(({ id, title, image, category, price, description }) => (
             <div className="product" key={id}>
               <img src={image} />
               <p>{title}</p>
               <h1>${price}</h1>
+              <p>
+                {readMore ? description : `${description.substring(0, 200)}...`}
+                <button onClick={() => setReadMore(!readMore)}>
+                  {readMore ? `show less` : `read more`}
+                </button>
+              </p>
               <sub>{category}</sub>
             </div>
           ))
